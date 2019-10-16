118dbbe4e468083cd9ee5a6963872aa340642dca
Dev Sebastian <devsebastian@gmail.com>
2019-10-16 19:32:40 +0530

    gave the todo list an all new look

--- tdl.js
+++ tdl.js
@@ -1,20 +1,19 @@
-$("ul").on("click","li",function(){
-    $(this).toggleClass("cutoff");
+$("ul").on("click", "input", function() {
+    $(this).siblings().toggleClass("cutoff");
 });
 
-$("ul").on("click","span",function(event){
-    $(this).parent().fadeOut(500,function(){
+$("ul").on("click", "span", function(event) {
+    $(this).parent().fadeOut(500, function() {
         $(this).remove();
     })
     event.stopPropagation();
 })
 
-$("input[type = 'text']").keypress(function(event){
-    if(event.which == 13){
-        var todoText = $(this).val();
-        $(this).val("");
-
-        $("ul").append("<li><span>X<span> " + todoText + "</li>");
-    }
-})
-// THIS IS UNSTYLED
+$("input[type = 'text']").keypress(function(event) {
+        if (event.which == 13) {
+            var todoText = $(this).val();
+            $(this).val("");
+            $("ul").append("<li><input type=\"checkbox\">" + todoText + "</li>");
+        }
+    })
+    // THIS IS UNSTYLED
